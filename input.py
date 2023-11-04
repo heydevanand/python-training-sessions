6fdad8847215be9679f89243d89e7556750476d3
Dev Anand <ananddev681@gmail.com>
2023-11-04 17:06:05 +0530

    session 3

---
+++ input.py
@@ -0,0 +1,6 @@
+name = input("What's your name? ")
+
+name = name.strip()
+
+name = name.title()
+print(f"hello, {name}", end="$")
---
+++ integers.py
@@ -0,0 +1,6 @@
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+z = int(x / y)
+
+print(z)
